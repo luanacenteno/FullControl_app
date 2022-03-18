7ba71ffecf95c09e8cc082c4f8cf976e4c1b4664
Luana Centeno <luana.centeno99@gmail.com>
2022-03-18 13:38:16 -0300

    fix port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 
 app.listen(port, () => {
   console.log(`fullcontrol-db Rest API listening on port ${port}`);
